859eaf647763e5003cea67aba28c39304a483ce2
Mario1294 <mario94arc@gmail.com>
2021-09-04 15:24:46 -0300

    Clase empleado creada y nlog implementado

---
+++ TP02/TP02/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TP02.Models;
+using System.IO;
+
+namespace TP02.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog injected into HomeController");
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Hello, this is the index!");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public string empleado(string nYa, int dni, DateTime fechaNac, string direccion, DateTime fechaIngr, string cargo)
+        {
+            try
+            {
+                Empleados miEmpleados = new Empleados();
+                miEmpleados.crearEmpleado(nYa, fechaNac, dni, direccion, fechaIngr, cargo);
+                return "Apellido y Nombre: " + miEmpleados.ApellidoYnombre + "Edad: " + miEmpleados.Edad + "Antiguedad: " + miEmpleados.Antiguedad + "Salario: " + miEmpleados.Salario;
+            }
+            catch (Exception Error)
+            {
+                return Error.Message;
+            }
+         }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TP02/TP02/Empleados.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP02
+{
+    public class Empleados
+    {
+        private string apellidoYnombre;
+        private DateTime fechaNac;
+        private int dni;
+        private string direccion;
+        private DateTime ingreso;
+        private string datosProfecionales;
+        private int edad;
+        private int antiguedad;
+        private double salario;
+
+
+        public string ApellidoYnombre { get => apellidoYnombre; set => apellidoYnombre = value; }
+        public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public DateTime Ingreso { get => ingreso; set => ingreso = value; }
+        public string DatosProfecionales { get => datosProfecionales; set => datosProfecionales = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public int Antiguedad { get => antiguedad; set => antiguedad = value; }
+        public double Salario { get => salario; set => salario = value; }
+        public int Dni { get => dni; set => dni = value; }
+        
+
+        public void crearEmpleado(string _apellidoYnombre, DateTime _fechaNac, int _dni, string _direccion, DateTime _ingreso, string _datosProfecionales)
+        {
+            this.ApellidoYnombre = _apellidoYnombre;
+            this.fechaNac = _fechaNac;
+            this.dni = _dni;
+            this.direccion = _direccion;
+            this.Ingreso = _ingreso;
+            this.datosProfecionales = _datosProfecionales;
+
+            //---Edad Y Antiguedad----//
+            this.edad = DateTime.Today.AddTicks(-FechaNac.Ticks).Year - 1;
+            this.antiguedad = DateTime.Today.AddTicks(-ingreso.Ticks).Year - 1;
+
+            //-----Salario-----//
+            double sueldoBasico = 28000;
+            double adicional;
+            double descuento = (sueldoBasico * 15) / 100;
+
+            if (antiguedad >= 1 && antiguedad < 20)
+            {
+                adicional = (sueldoBasico * antiguedad) / 100;
+                salario = sueldoBasico + adicional - descuento;
+            }
+            if (antiguedad >= 20)
+            {
+                adicional = (sueldoBasico * 25) / 100;
+                salario = sueldoBasico + adicional - descuento;
+            }
+            else
+            {
+                salario = sueldoBasico - descuento;
+            }
+        }
+    }
+}
